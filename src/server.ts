e4515598f7fdab64aabb5d4b8f1e3222e8997f8c
Pedro Muniz <pedrolehimuniz@gmail.com>
2023-12-09 01:37:41 -0300

    DB connection to server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import { sequelize } from "./database";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  sequelize.authenticate().then(() => {
+    console.log("DB connected.");
+  });
   console.log(`Server started at port ${PORT}`);
 });
