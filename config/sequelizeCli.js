d562cb622626c041d1702831fa99b473d11ae007
Pedro Muniz <pedrolehimuniz@gmail.com>
2023-12-09 01:35:07 -0300

    sequelize-postgres config

---
+++ config/sequelizeCli.js
@@ -0,0 +1,10 @@
+module.exports = {
+	development: {
+		dialect: 'postgres',
+		host: 'localhost',
+		port: '5432',
+		database: 'streaming_videos_development',
+		username: 'pedro',
+		password: 'pedro'
+	}
+}
---
+++ src/database/index.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize({
+  dialect: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "streaming_videos_development",
+  username: "pedro",
+  password: "pedro",
+  define: {
+    underscored: true, //facilitates converting SQL snake_case pattern to camelCase on JS.
+  },
+});
