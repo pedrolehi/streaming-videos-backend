272f652a58ee1e838b4395b62f51aaeb6c443fb6
Pedro Muniz <pedrolehimuniz@gmail.com>
2023-12-11 11:50:55 -0300

    Revert "admin panel"
    This reverts commit abd6da7dccdcbd8be2ada61adf81885ef3c1055f.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
 import express from "express";
 import { sequelize } from "./database";
-import { adminJs, adminJsRouter } from "./admin";
 
 const app = express();
-
-app.use(express.static("public")); // to serve the content in public as a static content.
-app.use(adminJs.options.rootPath, adminJsRouter); // path, route
 
 const PORT = process.env.PORT || 3000;
 
