80efbdece6b23efc65d6df8347b1dd805ba8af64
Pedro Muniz <pedrolehimuniz@gmail.com>
2023-12-21 18:27:17 -0300

    authMiddleware

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { categoriesController } from "./controllers/categoriesController";
 import { coursesController } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
 import { authController } from "./controllers/authController";
+import { ensureAuth } from "./middlewares/auth";
 
 const router = express.Router();
 
@@ -12,14 +13,14 @@ router.post("/auth/register", authController.register);
 router.post("/auth/login", authController.login);
 
 // Categories
-router.get("/categories", categoriesController.index);
-router.get("/categories/:id", categoriesController.show);
+router.get("/categories", ensureAuth, categoriesController.index);
+router.get("/categories/:id", ensureAuth, categoriesController.show);
 
 // Courses
-router.get("/courses/featured", coursesController.featured);
+router.get("/courses/featured", ensureAuth, coursesController.featured);
 router.get("/courses/newest", coursesController.newest);
-router.get("/courses/search", coursesController.search);
-router.get("/courses/:id", coursesController.show);
+router.get("/courses/search", ensureAuth, coursesController.search);
+router.get("/courses/:id", ensureAuth, coursesController.show);
 
 // Episodes
 router.get("/episodes/stream", episodesController.stream);
