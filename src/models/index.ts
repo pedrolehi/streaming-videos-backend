40464c24dba1f74ea0cafccd6216509f35507a0b
Pedro Muniz <pedrolehimuniz@gmail.com>
2024-01-03 17:33:45 -0300

    POST Likes create API

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,12 +3,14 @@ import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
 import { Favorite } from "./Favorite";
+import { Like } from "./Like";
 
 Category.hasMany(Course, { as: "courses" });
 
 Course.belongsTo(Category);
 Course.hasMany(Episode, { as: "episodes" });
 Course.belongsToMany(User, { through: Favorite });
+Course.belongsToMany(User, { through: Like });
 Course.hasMany(Favorite, { as: "FavoriresUsers", foreignKey: "course_id" });
 
 Episode.belongsTo(Course);
@@ -16,6 +18,7 @@ Episode.belongsTo(Course);
 Favorite.belongsTo(Course);
 
 User.belongsToMany(Course, { through: Favorite });
+User.belongsToMany(Course, { through: Like });
 User.hasMany(Favorite, { as: "FavoritesCourses", foreignKey: "user_id" });
 
-export { Category, Course, Episode, Favorite, User };
+export { Category, Course, Episode, Favorite, User, Like };
