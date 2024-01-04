69d0457f1a202aa5842bbf43c221c4bc4a5e1ee3
Pedro Muniz <pedrolehimuniz@gmail.com>
2024-01-04 12:42:44 -0300

    GET user Info API

--- src/routes.ts
+++ src/routes.ts
@@ -49,6 +49,7 @@ router.post("/likes", ensureAuth, likesController.save);
 router.delete("/likes", ensureAuth, likesController.delete);
 
 // Users
+router.get("/users/current", ensureAuth, usersController.show);
 router.get("/users/current/watching", ensureAuth, usersController.watching);
 
 export { router };
